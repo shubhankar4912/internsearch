3fdb12ef2c72d8c1299165234524769d31038278
Shubhankar Yadav <shubhankaryadav4912@gmail.com>
2024-12-05 21:51:08 +0530

    searchboxadded

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react"
 
 function SearchBox() {
+  const [searchtext,setsearch]=useState("");  
   return (
     <div className=" w-screen h-screen flex justify-center absolute top-32">
-      here box will come to make search
+        <div><input placeholder="search blogs"></input></div>
+      
     </div>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,7 @@
+const data=[
+    { id: 1, title: "React Basics", category: "React", date: "2023-10-01" },
+    { id: 2, title: "Next.js Guide", category: "Next.js", date: "2023-09-15" },
+    { id: 3, title: "Introduction to Tailwind CSS", category: "CSS", date: "2023-08-20" },
+    { id: 4, title: "JavaScript Tips", category: "JavaScript", date: "2023-07-12" },
+  ]
+  export default data;
